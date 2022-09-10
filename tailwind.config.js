28e3c77fbad63b1c8baf9b18cfafa4506dbb96b0
Jordi Irawan <jordirwn@gmail.com>
2022-09-10 10:01:15 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
@@ -6,6 +7,9 @@ module.exports = {
       container: {
         center: true,
         padding: "1rem",
+      },
+      fontFamily: {
+        sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
       },
     },
   },
